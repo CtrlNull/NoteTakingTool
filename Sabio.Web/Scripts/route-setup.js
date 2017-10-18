2ed04c3023e1d9759b80964770d0282d5882b282
Dan Farino <dan@sabio.la>
2017-10-18 11:28:09 -0700

    Add notes about UI Router

--- Sabio.Web/Scripts/route-setup.js
+++ Sabio.Web/Scripts/route-setup.js
@@ -9,6 +9,14 @@
     function configRoutes($stateProvider) {
         // All route definitions go in this function here
 
+        /*
+        Example URLs for entities:
+
+        /admin/entity/tags (view all tags screen)
+        /admin/entity/tags/123 (edit a specific tag, the route is actually defined as /admin/entity/tags/{id})
+        /admin/entity/tags/new (screen to create a new tag)
+        */
+
         $stateProvider.state({
             name: 'edit-exampleentity',
             url: '/admin/entity/example',
