fcd3ea9f2c2c2bc303609eac22dcc027dd6c3964
Ralph_Fonz <rfonsea@antioch.edu>
2017-10-19 17:37:47 -0700

    finished routing, added services for cs files

---
+++ Sabio.Models/Requests/TokenRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Requests
+{
+    public class TokenRequest
+    {
+        [Required]
+        [MinLength(1), MaxLength(50)]
+        public int id { get; set; }
+        public string service_name { get; set; }
+        public long token { get; set; }
+    }
+}
--- Sabio.Services/token-service.cs
+++ Sabio.Services/token-service.cs
@@ -1,7 +1,11 @@
 ﻿using Sabio.Data.Providers;
+using Sabio.Models.Domain;
+using Sabio.Models.Requests;
 using Sabio.Services.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,19 +22,63 @@ namespace Sabio.Services
             // store the paramenter in the field
             this.dataProvider = dataProvider;
         }
-        public int Create(token_create_request)
+        public int Create(TokenRequest request)
         {
             int id = 0;
             dataProvider.ExecuteNonQuery(
-            "token_create", inputParamMapper: delegate (SqlParameterCollection parametes)
+            "token_create", inputParamMapper: delegate (SqlParameterCollection parameters)
             {
-                paramenters.AddWithValue("@id", request.id);
-                parameters.AddwithValue("@service_name", request.service_name);
+                parameters.AddWithValue("@id", request.id);
+                parameters.AddWithValue("@service_name", request.service_name);
 
-                SqlParameter idParam = parameters.Add("@id").Value;
+                SqlParameter idParam = parameters.Add("@id", SqlDbType.Int);
+                idParam.Direction = ParameterDirection.Output;
+            },
+            returnParameters: delegate (SqlParameterCollection paramenters)
+            {
+                id = (int)paramenters["@id"].Value;
             });
+            return id;
         }
+        public List<Token> GetAll()
+        {
+            List<Token> results = null;
+            dataProvider.ExecuteCmd(
+               "third_party_token_getAll",
+               inputParamMapper: null,
+               singleRecordMapper: delegate (IDataReader reader, short set)
+               {
+                   if (results == null)
+                   {
+                       results = new List<Token>();
+                   }
+                   Token result = new Token();
+                   result.id = reader.GetInt32(0);
+                   result.service_name = reader.GetString(1);
+                   result.token = reader.GetInt32(2); // check! referenced as long!!!!
 
-
+                   results.Add(result);
+               });
+            return results;
+        }
+        public Token GetById(int id)
+        {
+            Token result = null;
+            dataProvider.ExecuteCmd(
+                "third_party_token_getbyid",
+                inputParamMapper: delegate (SqlParameterCollection parameters)
+                {
+                    parameters.AddWithValue("@id", id);
+                },
+                singleRecordMapper: delegate (IDataReader reader, short set)
+                {
+                    result = new Token();
+                    result.id = id;
+                    result.service_name = reader.GetString(1);
+                    result.token = reader.GetInt32(2);
+                }
+            );
+            return result;
+        }
     }
 }
