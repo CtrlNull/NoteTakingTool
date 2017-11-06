de9308bf9b6faae9b99a9a39ed515ac58fb5acdb
Ralph_Fonz <rfonseca@antioch.edu>
2017-11-06 13:18:57 -0800

    created a paste image controller and html page. now linking files together

--- Sabio.Web/Scripts/components/note-editor-paste/main.js
+++ Sabio.Web/Scripts/components/note-editor-paste/main.js
@@ -1,19 +1,38 @@
-(function(){
+(function () {
     'use strict';
-
-    angular.module(APPNAME)
-        .component('noteEditorText', {
-            templateUrl: '/Scripts/components/note-editor-text/main.html',
-            controller: 'EditorTextPasterController',
+    // Name *TextPaster*
+    angular.module('BananaPad')
+        .component('noteEditorImagePaster', {
+            templateUrl: '/Scripts/components/note-editor-image-paster/main.html',
+            controller: 'EditorImagePasterController',
             bindings: {
                 note: '<',
                 onSave: '&'
             }
         });
 
-    angular.module(APPNAME)
-        .controller('EditorTextPasterController', EditorTextController);
+    angular.module('BananaPad')
+        .controller('EditorImagePasterController', EditorImagePasterController);
 
-    function EditorTextController(){
+    EditorImagePasterController.$inject = ['$scope', 'Upload', '$timeout'];
+
+    function EditorImagePasterController($scope, Upload, $timeout) {
+        var vm = this;
+        $scope.upload = function (dataUrl, name) {
+            Upload.upload({
+                url: 'https://angular-file-upload-cors-srv.appspot.com/upload'
+                , data: { file: Upload.dataUrltoBlob(dataUrl, name) }
+            }).then(function (response) {
+                $timeout(function () {
+                    $scope.result = response.data;
+                });
+            },
+                function (response) {
+                    if (response.status > 0) $scope.errorMesg = response.status + ': ' + response.data;
+                },
+                function (evt) {
+                    $scope.progress = parseInt(100.0 * evt.loaded / evt.total);
+                });
+        }
     }
 })();
