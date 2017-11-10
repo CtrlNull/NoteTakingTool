64a1abe95dba39399517f45697e34c96210149f9
Ralph_Fonz <rfonseca@antioch.edu>
2017-11-10 15:17:02 -0800

    changed AUTH html/js/controller/service location

---
+++ Sabio.Web/Scripts/components/login-auth/main.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    'use strict'
+    angular.module(APPNAME)
+        .component('adminLoginGitController', {
+            templateUrl: '/Scripts/components/note-card/githubLogin.html'
+            , controller: 'adminLoginGitController'
+        });
+    angular.module(APPNAME)
+        .controller('adminLoginGitController', adminLoginGitController);
+    adminLoginGitController.$inject = ['loginGitService', '$stateParams'];
+
+    function adminLoginGitController(loginGitService, $stateParams) {
+        var vm = this;
+    }
+})();
---
+++ Sabio.Web/Scripts/services/loginService.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    'use strict'
+    angular.module(APPNAME)
+        .service('loginGitService', loginGitService);
+    loginGitService.$inject = ['$http'];
+
+    function loginGitService($http) {
+        var svc = this;
+        // hoist //
+        svc.create = _create;
+
+        //------ {Settings} ------ //
+        //~~<Create>
+        function _sendKey() {
+            return $http({
+                method: 'POST'
+                , url: 'http://github.com/login/oauth/authorize' + 
+            })
+        }
+
+
+
+    }
+
+
+})();
