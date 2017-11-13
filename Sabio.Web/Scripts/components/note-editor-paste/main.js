0e3538f59e591ba904a39518e9cc8dc26fd4b33c
Ralph_Fonz <rfonseca@antioch.edu>
2017-11-13 15:45:52 -0800

    fixed module issue and redid my card. now need the image to paste on the page

--- Sabio.Web/Scripts/components/note-editor-paste/main.js
+++ Sabio.Web/Scripts/components/note-editor-paste/main.js
@@ -3,7 +3,7 @@
     // Name *TextPaster*
     angular.module('BananaPad')
         .component('noteEditorPaste', {
-            templateUrl: '/Scripts/components/note-editor-image-paster/main.html',
+            templateUrl: '/Scripts/components/note-editor-paste/main.html',
             controller: 'EditorImagePasterController',
             bindings: {
                 note: '<',
@@ -18,21 +18,21 @@
 
     function EditorImagePasterController($scope, Upload, $timeout) {
         var vm = this;
-        $scope.upload = function (dataUrl, name) {
+        $scope.Upload = function (dataUrl, name) {
             Upload.upload({
-                url: 'https://angular-file-upload-cors-srv.appspot.com/upload'
-                , data: { file: Upload.dataUrltoBlob(dataUrl, name) }
+                url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
+                data: {
+                    file: Upload.dataUrltoBlob(dataUrl, name)
+                },
             }).then(function (response) {
                 $timeout(function () {
                     $scope.result = response.data;
                 });
-            },
-                function (response) {
-                    if (response.status > 0) $scope.errorMesg = response.status + ': ' + response.data;
-                },
-                function (evt) {
-                    $scope.progress = parseInt(100.0 * evt.loaded / evt.total);
-                });
+            }, function (response) {
+                if (response.status > 0) $scope.errorMsg = response.status + ': ' + response.data;
+            }, function (evt) {
+                $scope.progress = parseInt(100.0 * evt.loaded / evt.total);
+            });
         }
     }
 })();
