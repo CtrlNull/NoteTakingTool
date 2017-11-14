a1dda7d40f6cddae0a1857a974303c860770eaba
Ralph_Fonz <rfonseca@antioch.edu>
2017-11-14 14:35:43 -0800

    started implementing indexed-db

--- Sabio.Web/Scripts/components/note-editor-paste/main.js
+++ Sabio.Web/Scripts/components/note-editor-paste/main.js
@@ -18,6 +18,45 @@
             $scope.$on("fileProgress", function (e, progress) {
                 $scope.progress = progress.loaded / progress.total;
             });
+            var vm = this;
+            vm.btnSave = _btnSave;
+
+            // IndexDB
+            var request = window.indexedDB.open('mydb.db', 1);
+            var db;
+            if (!window.indexedDB) {
+                window.alert("Your browser doesnt support latest stable version of indexedDB");
+            }
+            // Already exists
+            request.onsuccess = function (event) {
+                db = event.target.result;
+                console.log("in the data base");
+            };
+            // if existing
+            request.onerror = function (event) {
+                console.log("error with indexedDB");
+            };
+            // newly created db
+            request.onupgradeneeded = function (event) {
+                console.log("inside on upgrade");
+                db = event.target.result; // Gives read acces to db
+                var objectStore = db.createObjectStore("imageTbl", { keyPath: "imageTbl", autoIncrement:true }); // Adds new Field
+                // Create Table
+                objectStore.createIndex('imageName', 'imageName', { unique: false }); // Defines field and gives indexing capability
+                objectStore.createIndex('imageSub', 'imageSub', { unique: false }); // Subject
+            };
+            function _btnSave() {
+                console.log("btnSave");
+                var transaction = db.transaction(['imageTbl'], 'readwrite');
+                var objectStore = transaction.objectStore('imageTbl');
+
+                var randomData = { imageName: "Dev", stdSub: Math.random() };
+                var request = objectStore.add(randomData);
+                request.onsuccess = function (event) {
+                    console.log(event);
+                }
+            }
+
         });
 
         app.directive("ngFileSelect", function (fileReader, $timeout) {
